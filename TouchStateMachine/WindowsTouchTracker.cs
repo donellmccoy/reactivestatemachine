a89cc88f589d1d65d8b6d9b121d3a7f596bfa64d
Roman Rädle <roman.raedle@gmail.com>
2013-11-27 13:12:47 +0100

    Fixed state machine bug for WindowsTouchTracker when using StylusTouchDevice Update to latest NuGet packages

--- TouchStateMachine/WindowsTouchTracker.cs
+++ TouchStateMachine/WindowsTouchTracker.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Mime;
+using System.Reflection;
 using System.Windows.Input;
 using ReactiveStateMachine;
 
@@ -15,12 +17,39 @@ namespace TouchStateMachine
 
             var args = e as TouchEventArgs;
 
-            var contactAction = args.TouchDevice.GetTouchPoint(null).Action;
+            var touchDevice = args.TouchDevice;
+
+            TouchAction contactAction;
+            try
+            {
+                contactAction = touchDevice.GetTouchPoint(null).Action;
+            }
+            catch (Exception)
+            {
+                contactAction = (TouchAction)GetInstanceField(touchDevice.GetType(), touchDevice, "_lastAction");
+            }
 
             if (contactAction == TouchAction.Down)
                 AddPoint(args.TouchDevice);
             else if (contactAction == TouchAction.Up)
                 RemovePoint(args.TouchDevice);
+        }
+
+        /// <summary>
+        /// Uses reflection to get the field value from an object.
+        /// </summary>
+        ///
+        /// <param name="type">The instance type.</param>
+        /// <param name="instance">The instance object.</param>
+        /// <param name="fieldName">The field's name which is to be fetched.</param>
+        ///
+        /// <returns>The field value from the object.</returns>
+        internal static object GetInstanceField(Type type, object instance, string fieldName)
+        {
+            BindingFlags bindFlags = BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic
+                | BindingFlags.Static;
+            FieldInfo field = type.GetField(fieldName, bindFlags);
+            return field.GetValue(instance);
         }
     }
 }
